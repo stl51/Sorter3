4d701c9e1ed4d2f9c8f1e82bdee5b3c26b372e85
Shaun T. Lim <stl51@strategy.cs.rutgers.edu>
2017-12-09 16:38:24 -0500

    adding file

---
+++ Serves_up.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+int main(int argc, char **argv){
+	//SERVE'S UP BIG KAHUNA!
+	
+	int server_sock;
+	int client_sock;
+	
+	
+	
+	return 0;
+}
